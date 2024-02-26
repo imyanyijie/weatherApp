97ddf5fc3a7d69eb09a86d633f43295663b8d3ce
imyanyijie <yanyijie1993@gmail.com>
2024-02-26 18:26:53 -0500

    added necessary function to fetch weather data
    Added function to pull data using weather API
    Create weather card section using the data from API base on the location user input
    added function to switch between farenheit and celcius

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './style.css';
+import { getWeather, createCard } from './model/weather';
 
+
+const apiKey = "";
+
+const searchBox = document.querySelector("#location");
+const searchButton = document.querySelector("#search");
+
+searchButton.addEventListener("click",function(event){
+    console.log("button clicked");
+    let weather = getWeather(searchBox.value);
+    console.log(weather);
+    createCard(weather);
+
+
+});
 
 console.log("test");
