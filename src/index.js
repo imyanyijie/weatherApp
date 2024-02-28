97cd2ed347626d5207f55a4887dc557379d37930
imyanyijie <yanyijie1993@gmail.com>
2024-02-28 09:36:59 -0500

    added roboto font and modeifty layout of the page
    added roboto font and set that as default font
    change the behavior of the header on window width shrink.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import './style.css';
+import './reset.css';
+
+
 import { getWeather, createCard } from './model/weather';
-
-
-const apiKey = "";
 
 const searchBox = document.querySelector("#location");
 const searchButton = document.querySelector("#search");
 
 searchButton.addEventListener("click",function(event){
     console.log("button clicked");
-    let weather = getWeather(searchBox.value);
-    console.log(weather);
-    createCard(weather);
-
-
+    getWeather(searchBox.value).then(function(weather){
+        console.log(weather);
+        createCard(weather)
+    });
 });
 
 console.log("test");
